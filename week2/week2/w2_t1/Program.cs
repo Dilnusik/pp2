9cb33dad05b42935b6ea62988d3573b3d4c658be
Dilyoka <ltvdlnz@mail.ru>
2019-02-15 01:44:56 +0600

    second

---
+++ week2/week2/w2_t1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream(@"C:\Users\ww\Desktop\pp2\week2\file.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sw = new StreamReader(fs);
+            if (IsPal(sw.ReadLine()))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+        static bool IsPal(string x)
+        {
+            for (int i = 0; i < x.Length / 2; ++i)
+            {
+                if (x[i] != x[x.Length - 1 - i]) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ week2/week2/w2_t4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fs = new FileStream(@"C:\Users\ww\Desktop\pp2\week2\path\newf.txt", FileMode.Create)) { } //создал файл, если сущ то перезаписывается
+
+            string fileName = "newf.txt";  //сам файл который долж скопироаваться
+            string path = @"C:\Users\ww\Desktop\pp2\week2\path\";      //источник
+            string path1 = @"C:\Users\ww\Desktop\pp2\week2\path1\";    //цель
+
+            string Ffile = Path.Combine(path, fileName);  // сведения о пути
+            string Sfile = Path.Combine(path1, fileName);
+
+
+            File.Copy(Ffile, Sfile, true);   //копирование
+            File.Delete(@"C:\Users\ww\Desktop\pp2\week2\path\newf.txt"); // удаление оригинала
+
+        }
+    }
+}
